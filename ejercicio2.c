2b8c9862bd9740e834ffccb87faa401c777fe9ca
MontseBravo <montsebravo0105@gmail.com>
2025-04-06 17:50:12 -0600

    ejercicio2.c

---
+++ ejercicio2.c
@@ -0,0 +1,19 @@
+//clasificador de triangulos//
+#include <stdio.h>
+#include <string.h>
+int main(){
+    int lado1, lado2, lado3;
+    char tipo[15];
+    printf("Ingrese los 3 lados del triángulo:");
+    scanf("%d  %d  %d", &lado1,&lado2,&lado3);
+    if(lado1 + lado2 > lado3 && lado2+lado3 >lado1 && lado1 +lado3 >lado2){
+       if(lado1 = lado2 || lado1 = lado3 || lado2 = lado3){
+          strcpy(tipo,"isoceles"); 
+       }
+    }else if (lado1 = lado2 && lado2 = lado3){
+        strcpy(tipo,"equilatero");
+    }else {
+        strcpy(tipo,"escaleno");
+    }
+    return 0;
+}
